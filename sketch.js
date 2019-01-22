206f14a39b27a430aea718dcf455402254332c1b
umetaman <info@umetaman.com>
2019-01-22 12:41:26 +0900

    first commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+}
